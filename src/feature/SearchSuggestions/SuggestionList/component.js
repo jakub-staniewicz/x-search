518767dc6547fbf9fb088fe2aa3693477fbc9a67
Jakub Staniewicz <jakub.staniewicz@gmail.com>
2023-05-31 20:33:07 +0200

    loupe added

--- src/feature/SearchSuggestions/SuggestionList/component.js
+++ src/feature/SearchSuggestions/SuggestionList/component.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+const getClassString =(selected, recentSearch) => `${selected ? 'selected' : ''} ${recentSearch ? 'recentSearch' : ''}`;
+               
+export const SuggestionsList = ({ suggestions, selectedSearchSuggestionIndex, onDelete, onClick }) => {
+    const [onHover, setOnHover] = useState(false);
 
-export const SuggestionsList = ({ suggestions, selectedSearchSuggestionIndex, onDelete, onClick }) => <>
-    {suggestions?.length > 0 && <ul className="suggestionsList" tabIndex="0">
-        {suggestions?.map((suggestion, index) => {
-            const selected = selectedSearchSuggestionIndex === index;
-            return (
-                <li
-                    className={selected ? 'selected' : undefined}
-                    key={suggestion.search}
-                    onClick={() => { onClick(suggestion, index) }}
-                >
-                    {suggestion.search}
-                    {suggestion.fromSearchHistory && selected && <button onClick={(e) => {
-                        onDelete(suggestion, e);
-                    }}>delete</button>}
-                </li>
-            )
-        })}
-    </ul >}
-</>;
+    return <>
+        {suggestions?.length > 0 && <ul className="suggestionsList" tabIndex="0">
+            {suggestions?.map((suggestion, index) => {
+                const selected = selectedSearchSuggestionIndex === index;
+                const recentSearch = !!suggestion.fromSearchHistory;
+
+                 return (
+                    <li
+                        className={getClassString(selected, recentSearch)}
+                        key={suggestion.search}
+                        onClick={() => { onClick(suggestion, index) }}
+                    >
+                        {suggestion.search}
+                        {recentSearch && <button onClick={(e) => {
+                            onDelete(suggestion, e);
+                        }}>delete</button>}
+                    </li>
+                )
+            })}
+        </ul >}
+    </>
+};
